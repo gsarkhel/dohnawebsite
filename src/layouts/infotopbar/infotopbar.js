e8c297051aca16966d40472c25c967b41ef00cc2
Govinda Sarkhel <gsarkhel@gmail.com>
2021-04-25 12:26:01 +0530

    In Progress

---
+++ src/layouts/infotopbar/infotopbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class InfoTopBar extends React.Component {
+    render() {
+        return(
+            <div className="infotopbar">
+                <div className="infotopbarinner">
+                    Info Top Bar
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/main/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Home from '../homepage/home';
+export default class App extends React.Component {
+    render() {
+        return (
+            <div><Home/></div>
+        );
+    }
+}
